e51a2f63c6cdaf8fff73d30b8eeb0f7d025d14a2
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-10-08 23:27:46 -0300

    changes pokedex-container from grid to flewbox

--- src/pokedex.js
+++ src/pokedex.js
@@ -8,21 +8,21 @@ const previousButton = document.getElementById('previous')
 let searchIndex = 1;
 
 nextButton.onclick = () => {
-    searchIndex += 24;
+    searchIndex += 25;
     render(searchIndex);
 }
 previousButton.onclick = () => {
     if (searchIndex === 1) return;
     
-    searchIndex -= 24;
+    searchIndex -= 25;
     render(searchIndex);
 }
 
 async function render(index){
-    previousButton.disabled = searchIndex === 1 || searchIndex === 42; //Disable button if it's the first page
+    previousButton.disabled = searchIndex === 1 || searchIndex === 40; //Disable button if it's the first page
     try {
         pokedex.replaceChildren();
-        for (let i = index; i < index + 24; i++){
+        for (let i = index; i < index + 25; i++){
             const data = await fetchPokemonData(i);
             const pokemon = new Pokemon(data);
             pokedex.appendChild(pokemon.createElement())

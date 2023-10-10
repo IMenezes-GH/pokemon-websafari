09a894c783eefa7577d587af458d586d038031e4
IMenezes <107199192+IMenezes-GH@users.noreply.github.com>
2023-10-10 18:47:58 -0300

    removes console.log line

--- src/dialog.js
+++ src/dialog.js
@@ -141,8 +141,6 @@ export class DialogPokemon{
         const [type1, type2] = [...this.pokemon.types];
         const [hp, atk, def, spa, spd, spe] = [...this.pokemon.stats];
         const [ab1, ab2, ha] = [...this.pokemon.abilities];
-        
-        console.log(this.pokemon);
 
         dialog.classList.add('pokemon-dialog')
         dialog.innerHTML =
